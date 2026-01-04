f578bc280746a440c35d2dd205034e2deaecfea1
sushant manjare <sushantmanjare17269@gmail.com>
2026-01-05 02:34:36 +0530

    Initial project setup with User entity

--- banking-core/src/main/java/com/bankapp/repository/UserRepository.java
+++ banking-core/src/main/java/com/bankapp/repository/UserRepository.java
@@ -3,9 +3,9 @@ package com.bankapp.repository;
 import com.bankapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
-
 }
